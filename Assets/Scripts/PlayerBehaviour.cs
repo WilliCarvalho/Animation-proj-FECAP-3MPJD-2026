d10c00f07e9d83ef4851c75846d1911f523117dd
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2026-04-23 09:45:02 -0300

    Started animation controller system

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    
+    private InputManager inputManager;
+
+    private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        inputManager = new InputManager();
+        rigidbody = GetComponent<Rigidbody>();
+        inputManager.OnAttackPressed += HandleAttackBehaviour;
+    }
+
+
+    private void FixedUpdate()
+    {
+        float moveX = inputManager.GetInputDirection().x * moveSpeed * Time.deltaTime;
+        float moveZ = inputManager.GetInputDirection().y * moveSpeed * Time.deltaTime;
+        rigidbody.linearVelocity = new Vector3(moveX, rigidbody.linearVelocity.y, moveZ);
+    }
+    
+    #region Referência de implementação - Não será utilizado no projeto
+    private void HandleAttackBehaviour()
+    {
+        print("Handling ATTACK with strength ");
+    }
+#endregion
+
+}
