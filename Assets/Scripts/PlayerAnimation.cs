4d80efce1ee9f11fcd6b352d18917b81a2783acc
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2026-05-07 10:17:36 -0300

    Created Animation Controller
    - Started creating Third Person Controller

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    //Pode guardar o valor do parâmetro de animação numa variável
+    //private int IsMoving = Animator.StringToHash("isMoving");
+
+    private string isMovingParam = "isMoving";
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    public void SetIsMoving(bool isMoving)
+    {
+        animator.SetBool(isMovingParam, isMoving);
+    }
+}
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,31 +4,93 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationVelocity;
     
     private InputManager inputManager;
-
+    private PlayerAnimation playerAnim;
     private Rigidbody rigidbody;
+    
+    private Vector3 moveDirection;
 
     private void Awake()
     {
         inputManager = new InputManager();
+        playerAnim = GetComponent<PlayerAnimation>();
         rigidbody = GetComponent<Rigidbody>();
-        inputManager.OnAttackPressed += HandleAttackBehaviour;
     }
-
 
     private void FixedUpdate()
     {
-        float moveX = inputManager.GetInputDirection().x * moveSpeed * Time.deltaTime;
-        float moveZ = inputManager.GetInputDirection().y * moveSpeed * Time.deltaTime;
-        rigidbody.linearVelocity = new Vector3(moveX, rigidbody.linearVelocity.y, moveZ);
+        Debug.Log("Linear Velocity Y: " + rigidbody.linearVelocity.y);
+        HandleMove();
+
+        CheckMovingForAnimation();
     }
     
-    #region Referência de implementação - Não será utilizado no projeto
-    private void HandleAttackBehaviour()
+    private void HandleMove()
     {
-        print("Handling ATTACK with strength ");
-    }
-#endregion
+        float moveX = inputManager.GetInputDirection().x * moveSpeed * Time.deltaTime;
+        float moveZ = inputManager.GetInputDirection().y * moveSpeed * Time.deltaTime;
+        
+        moveDirection.x = moveX;
+        moveDirection.z = moveZ;
+        
+        Vector3 cameraRelativeMovement = ConvertMoveDirectionToCameraSpace(moveDirection);
 
+        rigidbody.linearVelocity = 
+            new Vector3(cameraRelativeMovement.x, rigidbody.linearVelocity.y, cameraRelativeMovement.z);
+        
+        RotatePlayerAccordingToInput(cameraRelativeMovement);
+    }
+
+    private void RotatePlayerAccordingToInput(Vector3 cameraRelativeMovement)
+    {
+        Vector3 pointToLookAt;
+        pointToLookAt.x = cameraRelativeMovement.x;
+        pointToLookAt.y = 0;
+        pointToLookAt.z = cameraRelativeMovement.z;
+
+        Quaternion currentRotation = transform.rotation;
+
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(pointToLookAt);
+
+            transform.rotation =
+                Quaternion.Slerp(currentRotation,
+                    targetRotation,
+                    rotationVelocity * Time.deltaTime);
+        }
+    }
+
+    private Vector3 ConvertMoveDirectionToCameraSpace(Vector3 moveDirection)
+    {
+        Vector3 cameraForward = Camera.main.transform.forward;
+        Vector3 cameraRight = Camera.main.transform.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        Vector3 cameraForwardZProduct = cameraForward * moveDirection.z;
+        Vector3 cameraRightXProduct = cameraRight * moveDirection.x;
+
+        Vector3 directionToMovePlayer =
+            cameraForwardZProduct + cameraRightXProduct;
+
+        return directionToMovePlayer;
+    }
+
+    private void CheckMovingForAnimation()
+    {
+        Vector3 linearVelocityMagnitudeXZ = new  Vector3(rigidbody.linearVelocity.x, 0, rigidbody.linearVelocity.z); 
+        Debug.LogWarning("Player Linear Velocity " +  linearVelocityMagnitudeXZ);
+        if (linearVelocityMagnitudeXZ.magnitude != 0.0f)
+        {
+            playerAnim.SetIsMoving(true);
+        }
+        else
+        {
+            playerAnim.SetIsMoving(false);
+        }
+    }
 }
